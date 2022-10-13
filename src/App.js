58e759336fd46b17fcee437750a6e3addfaf6ed5
Agustin Romero <agusromero0815@gmail.com>
2022-10-13 12:51:37 -0300

    React-Redux cinema

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
       <React.Fragment>
           <NavBar />
-          <Route exact path="/catalogo" component={Buscador} />
+          <Route exact path="/catalogos" component={Buscador} />
           <Route path="/favs" component={Favorites} />
           <Route path="/movie/:id" component={Movie} />
           <Route path="/" render={() => <footer>&copy; AR 2022</footer>} />
